7fa80667c3e9213e232e9e7669cecfe174f35b18
Ketan Vardekar <ketan@elegantmicroweb.com>
2025-03-08 13:21:29 +0530

    chnages

--- src/app/components/PricingCard.js
+++ src/app/components/PricingCard.js
@@ -60,7 +60,7 @@ export default function YogaPlans() {
         </p>
       </div>
 
-      
+
 
       <motion.div
         className="mt-4 bg-green-100 p-4 rounded-lg text-center"
@@ -72,6 +72,17 @@ export default function YogaPlans() {
           Book Before 31st March : <span className="text-red-500">40% Off</span>
         </h2>
       </motion.div>
+
+
+      <motion.div
+        className="mt-4 p-4 rounded-lg text-center bg-gradient-to-r from-pink-300 to-pink-500 text-black shadow-lg"
+        animate={{ y: [0, -5, 0] }}
+        transition={{ repeat: Infinity, repeatType: "reverse", duration: 0.6 }}
+      >
+        <h2 className="font-bold text-lg">🎉 Women's Day Special Offer 🎉</h2>
+        <p className="font-medium">Join with a friend & get <span className="font-bold">45% off</span> each!</p>
+      </motion.div>
+
 
       <h3 className="mt-6 font-semibold text-black">
         Online Classes
